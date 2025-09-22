dae4ea5b3f0d82869fecd0e9f6cd734339e7af10
Bondalapati Bhavishya <bhavishya@Bondalapatis-MacBook-Pro.local>
2025-09-22 19:01:06 -0400

    third commit

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -2,7 +2,13 @@
 
 import Link from "next/link";
 
-export default function Assignments() {
+interface AssignmentsProps {
+  params: { cid: string }; // dynamic course ID
+}
+
+export default function Assignments({ params }: AssignmentsProps) {
+  const { cid } = params;
+
   return (
     <div id="wd-assignments">
       {/* Search and Add Buttons */}
@@ -18,42 +24,42 @@ export default function Assignments() {
       {/* Assignment List */}
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/123" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/123`} className="wd-assignment-link">
             A1 - ENV + HTML
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/124" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/124`} className="wd-assignment-link">
             A2 - CSS Basics
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/125" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/125`} className="wd-assignment-link">
             A3 - JavaScript Fundamentals
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/126" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/126`} className="wd-assignment-link">
             A4 - React Components
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/127" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/127`} className="wd-assignment-link">
             A5 - Node.js Basics
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/128" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/128`} className="wd-assignment-link">
             A6 - Express Routing
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/129" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/129`} className="wd-assignment-link">
             A7 - MongoDB CRUD
           </Link>
         </li>
         <li className="wd-assignment-list-item">
-          <Link href="/Courses/1234/Assignments/130" className="wd-assignment-link">
+          <Link href={`/Courses/${cid}/Assignments/130`} className="wd-assignment-link">
             A8 - APIs Integration
           </Link>
         </li>
